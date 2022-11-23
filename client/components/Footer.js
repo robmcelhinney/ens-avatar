027918fe5c24f783f04ac1382109d3f9df30c9c9
Robert McElhinney <robmcelhinney@hotmail.com>
2022-11-23 17:36:22 +0000

    Add footer to web page

---
+++ client/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+function Footer() {
+	return (
+		<div id={"footer"}>
+			Any queries/requests, contact me @<a
+			href={"https://twitter.com/RMcElhinney"}>Rob McElhinney
+			</a> | Code Viewable @
+			<a href={
+					"https://github.com/robmcelhinney/ens-avatar"}>
+			github</a>
+		</div>
+	)
+}
+
+export default Footer
--- client/components/MyApp.js
+++ client/components/MyApp.js
@@ -1,5 +1,6 @@
 import { useConnect } from '../hooks/useConnect'
 import Homepage from '../pages/Homepage'
+import Footer from '../components/Footer'
 import Head from 'next/head'
 
 export const MyApp = () => {
@@ -7,12 +8,13 @@ export const MyApp = () => {
     useConnect()
   
     return (
-        <>
+        <div className={"flex flex-col h-screen justify-between"}>
         <Head>
             <title>ENS Avatar Updater</title>
             <meta name='description' content='Select your ENS Avatar from your owned NFTs' />
         </Head>
         <Homepage />
-        </>
+        <Footer />
+        </div>
     )
 }
